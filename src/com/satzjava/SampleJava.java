6e7d2837ad0389af514092f4cf669a40bff4924e
satzmech90 <satzmech90@gmail.com>
2021-06-09 23:18:29 +0530

    method added

--- src/com/satzjava/SampleJava.java
+++ src/com/satzjava/SampleJava.java
@@ -14,13 +14,19 @@ public class SampleJava {
 		System.out.println("Empolyee name " + name);
 		System.out.println("Mobile number "+ mobileNo);
 	}
+    private void empDetails(int salary, String DOB, String Address) {
+    	System.out.println("Salary of Employee is " +salary);
+    	System.out.println("DOB os Employee is " +DOB);
+    	System.out.println("Address of Employee is " +Address);
 
+	}
     public static void main(String[] args) {
 		SampleJava p=new SampleJava();
 		p.empDetails(100);
 		p.empDetails();
 		p.empDetails("surya", 8778531564L);
 		p.empDetails(1423, 1994);
+		p.empDetails(93565, "10.05.1990", "Konur, Dindigul");
 	}
 
 }
